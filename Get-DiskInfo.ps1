a35b00ff7ffe0735458d2613009902f8aa9950f4
Daniel Bourne <contact@danbourne.com>
2021-12-05 11:50:15 -0600

    Create Get-DiskInfo.ps1

---
+++ Get-DiskInfo.ps1
@@ -0,0 +1,37 @@
+function Get-DCDiskInfo
+{
+
+    $ADForest = (Get-ADForest).Domains
+    
+     foreach ($Domain in ((Get-ADForest).Domains)){
+
+        $Hosts = Get-ADDomainController -Filter * -Server $Domain | Sort-Object -Property Hostname
+
+            foreach($DCHost in $Hosts){
+
+                try
+                {
+                    $CS = Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $DCHost -ErrorAction Stop
+                    
+                    $Properties = @{'ComputerName' = $DCHost
+                    'DomainController' = $DCHost
+                    'Manufacturer' = $CS.Manufacturer
+                    'Model' = $CS.Model
+                    'TotalPhysicalMemory(GB)' = $CS.TotalPhysicalMemory /1GB}
+
+                    New-Object -Type PSObject -Property $Properties
+                }#try
+                catch [Microsoft.Management.Infrastructure.CimException]
+                {
+                    Write-Warning "Failed to resolve $DCHost on $ADForest - CIMException"
+                }#catch [Microsoft.Management.Infrastructure.CimException]
+
+                catch
+                {
+                    $Error[0].Exception
+                }#catch all
+    
+    New-Object -TypeName psobject -Property $Properties
+            }#foreach($Host in $Hosts)
+     }#foreach ($Domain in (Get-ADForest.Domains))
+}#function Get-DCDiskInfo
